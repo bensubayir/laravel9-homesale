e1c5e949415ce0987e35ec8c5a99d0cccba4d7ce
bensubayir <bensu.bayir@gmail.com>
2022-04-15 17:44:04 +0300

    template implementation

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,19 +1,56 @@
-<html lang="">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <title>Home Sale@yield('title')</title>
-    @yield('head')
+    <meta charset="utf-8">
+    <title>@yield("title")</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Inter:wght@700;800&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    @yield("head")
 </head>
+
 <body>
-<h1>homa sale </h1>
+<div class="container-xxl bg-white p-0">
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
 @section('sidebar')
-    This is the master sidebar.
+    @include("home.sidebar")
+@include("home.header")
+
 @show
 
-<div class="container">
-    @yield('content')
-</div>
-<h1>footer</h1>
-@yield('foot')
 
+    @yield('content')
+
+@include("home.footer")
+@yield('foot')
 </body>
 </html>
