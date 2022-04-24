7959fd178f0ae64fbf67f6b0b9d9fa064ded0d9c
bensubayir <bensu.bayir@gmail.com>
2022-04-24 19:45:08 +0300

    category insert data to database

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,10 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 //*******************ADMİN ROUTES************************
 Route::get('/admin',[AdminHomeController::class,'index'])->name('admin');
+
+//*******************ADMİN CATEGORY ROUTES************************
+Route::get('/admin/category',[\App\Http\Controllers\AdminPanel\CategoryController::class,'index'])->name('admin_category');
+
+Route::get('/admin/category/create',[\App\Http\Controllers\AdminPanel\CategoryController::class,'create'])->name('admin_category_create');
+Route::post('/admin/category/store',[\App\Http\Controllers\AdminPanel\CategoryController::class,'store'])->name('admin_category_store');
+
